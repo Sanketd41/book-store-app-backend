7617c225a4294ef72d830a9f65e8db94763c2d01
Sanket D41 <sanketdhakane24@gmail.com>
2025-12-09 16:13:27 +0530

    Delete

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -20,7 +20,7 @@ router.post("/admin", async (req, res) => {
         const token =  jwt.sign(
             {id: admin._id, username: admin.username, role: admin.role}, 
             JWT_SECRET,
-            {expiresIn: "1m"}
+            {expiresIn: "1h"}
         )
 
         return res.status(200).json({
